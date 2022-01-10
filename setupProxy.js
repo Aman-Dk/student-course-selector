451ee36fdbfdb7bcfc1ae888e2ddff48986ff72c
Aman Deep Kujur <amandeepkujur56@gmail.com>
2022-01-10 15:22:42 +0530

    Update setupProxy.js

--- setupProxy.js
+++ setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api/*',
+    '/*',
     createProxyMiddleware({
-      target: 'https://student-courseselector-backend.herokuapp.com/',
+      target: 'https://student-courseselector-backend.herokuapp.com',
       changeOrigin: true,
     })
   );
